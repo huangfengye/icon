986a41410d5b26f75b014fed784129468323d5fa
huangfengye <hhh583648414@gmail.com>
2023-05-17 00:36:45 +0800

    Update JKBL.js

--- JKBL.js
+++ JKBL.js
@@ -21,7 +21,7 @@ if (url. indexOf (mumu) != -1){
 	  
 	  },
 	  "ecode" : "200",
-	  "datetime" : "2023-05-06 23:01:49",  
+	  "datetime" : "2099-05-06 23:01:49",  
 	  "_local_" : "116"  
 };
 	body = JsON.stringify(objc);
